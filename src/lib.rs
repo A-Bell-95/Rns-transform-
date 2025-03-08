3489ac02aa9208f50542f7475f537a9d201eb77c
starovoid <prototyperailgun@gmail.com>
2025-03-08 18:12:07 +0300

    0 & 1 constants & Display

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
 pub mod convert;
 pub mod modulus;
+
+#[cfg(test)]
 pub mod tests;
+
+use std::fmt;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Rns<const N: usize> {
     remainders: [u8; N],
+}
+
+impl<const N: usize> Rns<N> {
+    pub const fn zero() -> Self {
+        Rns { remainders: [0; N] }
+    }
+
+    pub const fn one() -> Self {
+        Rns { remainders: [1; N] }
+    }
+}
+
+impl<const N: usize> fmt::Display for Rns<N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", &self.remainders[..N.min(18)])
+    }
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,5 +1,24 @@
-#[cfg(test)]
-pub mod convert;
-
-#[cfg(test)]
+mod convert;
 mod modulus;
+
+use crate::Rns;
+
+#[test]
+fn test_rns_zero() {
+    assert_eq!(
+        Rns::zero(),
+        Rns {
+            remainders: [0; 18]
+        }
+    )
+}
+
+#[test]
+fn test_rns_one() {
+    assert_eq!(
+        Rns::one(),
+        Rns {
+            remainders: [1; 18]
+        }
+    )
+}
