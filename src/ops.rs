8358a2373cf7d5b7862dd05930ad8c62d9171758
starovoid <prototyperailgun@gmail.com>
2025-03-09 21:00:13 +0300

    Impl Sub & SubAssign for Rns

--- src/ops.rs
+++ src/ops.rs
@@ -1,5 +1,5 @@
 use super::{Rns, PRIME_BASE_U64};
-use std::ops::{Add, AddAssign};
+use std::ops::{Add, AddAssign, Sub, SubAssign};
 
 impl<const N: usize> Add for Rns<N> {
     type Output = Self;
@@ -21,6 +21,35 @@ impl<const N: usize> AddAssign for Rns<N> {
             .enumerate()
         {
             self.remainders[i] = ((self.remainders[i] as u16 + r as u16) % (p as u16)) as u8;
+        }
+    }
+}
+
+impl<const N: usize> Sub for Rns<N> {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        let mut sum = self;
+        sum -= other;
+        sum
+    }
+}
+
+impl<const N: usize> SubAssign for Rns<N> {
+    fn sub_assign(&mut self, other: Self) {
+        for (i, (&r, &p)) in other
+            .remainders
+            .iter()
+            .zip(PRIME_BASE_U64.iter())
+            .take(N)
+            .enumerate()
+        {
+            let new_r = if self.remainders[i] >= r {
+                self.remainders[i] as u16 - r as u16
+            } else {
+                p as u16 - r as u16 + self.remainders[i] as u16
+            };
+            self.remainders[i] = new_r as u8;
         }
     }
 }
--- src/tests/ops.rs
+++ src/tests/ops.rs
@@ -64,3 +64,74 @@ fn test_add_assign_random() {
         assert_eq!(ra, Rns::new(expected));
     }
 }
+
+#[test]
+fn test_sub() {
+    let a = Rns::<18>::new(192);
+    let b = Rns::<18>::new(18);
+    assert_eq!(a - b, Rns::new(174));
+
+    let a = Rns::<18>::new(1);
+    let b = Rns::<18>::new(1);
+    assert_eq!(a - b, Rns::new(0));
+
+    let a = Rns::<18>::new(10100000);
+    let b = Rns::<18>::new(100000);
+    assert_eq!(a - b, Rns::new(10000000));
+}
+
+#[test]
+fn test_sub_random() {
+    let mut rng = rand::rng();
+
+    for _ in 0..100 {
+        let mut a = rng.next_u32();
+        let mut b = rng.next_u32();
+        if a < b {
+            std::mem::swap(&mut a, &mut b);
+        }
+        let ra = Rns::<18>::new(a as u64);
+        let rb = Rns::<18>::new(b as u64);
+        let res = ra - rb;
+        let expected = a as u64 - b as u64;
+        assert_eq!(res.into_u64(), expected);
+        assert_eq!(res, Rns::new(expected));
+    }
+}
+
+#[test]
+fn test_sub_assign() {
+    let mut a = Rns::<18>::new(192);
+    let b = Rns::<18>::new(18);
+    a -= b;
+    assert_eq!(a, Rns::new(174));
+
+    let mut a = Rns::<18>::new(1);
+    let b = Rns::<18>::new(1);
+    a -= b;
+    assert_eq!(a, Rns::new(0));
+
+    let mut a = Rns::<18>::new(10100000);
+    let b = Rns::<18>::new(10000000);
+    a -= b;
+    assert_eq!(a, Rns::new(100000));
+}
+
+#[test]
+fn test_sub_assign_random() {
+    let mut rng = rand::rng();
+
+    for _ in 0..100 {
+        let mut a = rng.next_u32();
+        let mut b = rng.next_u32();
+        if a < b {
+            std::mem::swap(&mut a, &mut b);
+        }
+        let mut ra = Rns::<18>::new(a as u64);
+        let rb = Rns::<18>::new(b as u64);
+        ra -= rb;
+        let expected = a as u64 - b as u64;
+        assert_eq!(ra.into_u64(), expected);
+        assert_eq!(ra, Rns::new(expected));
+    }
+}
