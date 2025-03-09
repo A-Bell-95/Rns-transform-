64b470a78bb53a60f32384c1a1ee5ea4236bf845
starovoid <prototyperailgun@gmail.com>
2025-03-09 20:37:15 +0300

    Impl AddAssign for Rns

--- src/ops.rs
+++ src/ops.rs
@@ -1,11 +1,18 @@
 use super::{Rns, PRIME_BASE_U64};
-use std::ops::Add;
+use std::ops::{Add, AddAssign};
 
 impl<const N: usize> Add for Rns<N> {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        let mut sum = self.remainders;
+        let mut sum = self;
+        sum += other;
+        sum
+    }
+}
+
+impl<const N: usize> AddAssign for Rns<N> {
+    fn add_assign(&mut self, other: Self) {
         for (i, (&r, &p)) in other
             .remainders
             .iter()
@@ -13,9 +20,7 @@ impl<const N: usize> Add for Rns<N> {
             .take(N)
             .enumerate()
         {
-            sum[i] = ((sum[i] as u16 + r as u16) % (p as u16)) as u8;
+            self.remainders[i] = ((self.remainders[i] as u16 + r as u16) % (p as u16)) as u8;
         }
-
-        Rns { remainders: sum }
     }
 }
--- src/tests/ops.rs
+++ src/tests/ops.rs
@@ -6,6 +6,14 @@ fn test_add() {
     let a = Rns::<18>::new(192);
     let b = Rns::<18>::new(18);
     assert_eq!(a + b, Rns::new(210));
+
+    let a = Rns::<18>::new(0);
+    let b = Rns::<18>::new(1);
+    assert_eq!(a + b, Rns::new(1));
+
+    let a = Rns::<18>::new(100000);
+    let b = Rns::<18>::new(10000000);
+    assert_eq!(a + b, Rns::new(10100000));
 }
 
 #[test]
@@ -20,5 +28,39 @@ fn test_add_random() {
         let res = ra + rb;
         let expected = a as u64 + b as u64;
         assert_eq!(res.into_u64(), expected);
+    }
+}
+
+#[test]
+fn test_add_assign() {
+    let mut a = Rns::<18>::new(192);
+    let b = Rns::<18>::new(18);
+    a += b;
+    assert_eq!(a, Rns::new(210));
+
+    let mut a = Rns::<18>::new(0);
+    let b = Rns::<18>::new(1);
+    a += b;
+    assert_eq!(a, Rns::new(1));
+
+    let mut a = Rns::<18>::new(100000);
+    let b = Rns::<18>::new(10000000);
+    a += b;
+    assert_eq!(a, Rns::new(10100000));
+}
+
+#[test]
+fn test_add_assign_random() {
+    let mut rng = rand::rng();
+
+    for _ in 0..100 {
+        let a = rng.next_u32();
+        let b = rng.next_u32();
+        let mut ra = Rns::<18>::new(a as u64);
+        let rb = Rns::<18>::new(b as u64);
+        ra += rb;
+        let expected = a as u64 + b as u64;
+        assert_eq!(ra.into_u64(), expected);
+        assert_eq!(ra, Rns::new(expected));
     }
 }
